9e988f39eb9a0ee0072cdfc87277fe34a93adf28
GHEUX Théo <theoxghx@gmail.com>
2025-11-25 23:02:08 +0100

    Ajout des constructeurs des romains

--- src/com/example/TD3/characters/roman/General.java
+++ src/com/example/TD3/characters/roman/General.java
@@ -1,4 +1,17 @@
 package com.example.TD3.characters.roman;
+
+import com.example.TD3.enums.Sex;
+
 // Généraux
 public class General extends Roman {
+
+    // Constructeur personnalisé
+    public General(String name, Sex sex, int size, int age, int strength, int stamina, int health) {
+        super(name, sex, size, age, strength, stamina, health);
+    }
+
+    // Constructeur avec des stats par défaut
+    public General(String name, Sex sex) {
+        super(name, sex, randomBetween(175,185), randomBetween(45,60),50,55,110);
+    }
 }
--- src/com/example/TD3/characters/roman/Legionary.java
+++ src/com/example/TD3/characters/roman/Legionary.java
@@ -1,4 +1,17 @@
 package com.example.TD3.characters.roman;
+
+import com.example.TD3.enums.Sex;
+
 // Romain légionnaires
 public class Legionary extends Roman{
+
+    // Constructeur personnalisé
+    public Legionary(String name, Sex sex, int size, int age, int strength, int stamina, int health) {
+        super(name, sex, size, age, strength, stamina, health);
+    }
+
+    // Constructeur avec des stats par défaut
+    public Legionary(String name, Sex sex) {
+        super(name, sex, randomBetween(170,180), randomBetween(20,35),60,75,110);
+    }
 }
