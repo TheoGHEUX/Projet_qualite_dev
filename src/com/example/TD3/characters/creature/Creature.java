e30d708a736362ab01c5551579b301c3007fba7c
GHEUX Théo <theoxghx@gmail.com>
2025-11-25 23:03:42 +0100

    Constructeurs temporaires des créatures

--- src/com/example/TD3/characters/creature/Creature.java
+++ src/com/example/TD3/characters/creature/Creature.java
@@ -1,6 +1,21 @@
 package com.example.TD3.characters.creature;
 
 import com.example.TD3.characters.Character;
+import com.example.TD3.enums.Sex;
+
+import java.util.Random;
 
 public class Creature extends Character {
+
+    // Variable utile à la génération de l'âge et la taille aléatoire
+    protected static final Random RANDOM = new Random();
+
+    public Creature(String name, Sex sex, int size, int age, int strength, int stamina, int health) {
+        super(name, sex, size, age, strength, stamina, health);
+    }
+
+    // Méthode pour générer aléatoirement un nombre dans un intervalle (utilisée pour générer la taille et l'âge)
+    protected static int randomBetween(int min, int max) {
+        return RANDOM.nextInt(max - min + 1) + min;
+    }
 }
