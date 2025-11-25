6da11b476c23310d22da93564d35b33052c4bfe4
GHEUX Théo <theoxghx@gmail.com>
2025-11-25 23:43:56 +0100

    Méthode eat() complète + ajout d'une varaible de classe maxHealth et last_was_vege

--- src/com/example/TD3/characters/Character.java
+++ src/com/example/TD3/characters/Character.java
@@ -1,6 +1,7 @@
 package com.example.TD3.characters;
 
 import com.example.TD3.enums.Sex;
+import com.example.TD3.food.Food;
 
 public abstract class Character {
     protected String name;
@@ -10,9 +11,11 @@ public abstract class Character {
     protected  int strength;
     protected  int stamina;
     protected  int health;
+    protected int maxHealth;
     protected  int hunger;
     protected  int belligerence ;
     protected  int potion_level;
+    protected  boolean last_was_vege;
 
     public Character(String name, Sex sex, int size, int age, int strength, int stamina, int health) {
         this.name = name;
@@ -22,9 +25,11 @@ public abstract class Character {
         this.strength = strength;
         this.stamina = stamina;
         this.health = health;
+        this.maxHealth = health;
         this.hunger = 100;
         this.belligerence = 0;
         this.potion_level = 0;
+        this.last_was_vege = false;
     }
 
     public void fight (Character enemy) {
@@ -35,8 +40,44 @@ public abstract class Character {
         this.health += sum;
     }
 
-    public void eat (int sum){
-        this.hunger += sum;
+    public void eat (Food food){
+
+        // Gestion de la faim qui ne peut pas être supérieure à 100
+        if((food.getHungerEffect() + hunger) >= 100){
+            this.hunger = 100;
+        }
+        else{
+            this.hunger += food.getHungerEffect();
+        }
+
+        // Gestion des végétaux : si deux végétaux d'affilée sont consommés, l'effet de vie est négatif
+        if(food.isVege()){
+            if(this.last_was_vege){
+                this.health -= Math.abs(food.getHealthEffect());
+                if(this.health <= 0){
+                    this.decease();
+                }
+            }
+            else{
+                this.last_was_vege = true;
+                if(this.health + food.getHealthEffect() > this.maxHealth){
+                    this.health = this.maxHealth;
+                }
+                else{
+                    this.health += food.getHealthEffect();
+                }
+            }
+        }
+        else{
+            this.last_was_vege = false;
+            if(this.health + food.getHealthEffect() > this.maxHealth){
+                this.health = this.maxHealth;
+            }
+            else{
+                this.health += food.getHealthEffect();
+            }
+        }
+
     }
 
     public void drinkMagicPotion (int doses){
@@ -44,6 +85,5 @@ public abstract class Character {
     }
 
     public void decease (){
-
     }
 }
