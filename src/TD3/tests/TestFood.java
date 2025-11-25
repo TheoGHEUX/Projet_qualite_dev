dd068266a820dac6367fb054519cf84c91a0bc4c
GHEUX Théo <theoxghx@gmail.com>
2025-11-26 00:12:51 +0100

    Ajouts de tests unitaires pour la nourriture

---
+++ src/TD3/tests/TestFood.java
@@ -0,0 +1,38 @@
+package TD3.tests;
+
+import TD3.food.Food;
+import TD3.food.FoodType;
+import TD3.food.FoodStats;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodTest {
+
+    @Test
+    void testConstructorAndGetters() {
+        Food food = new Food(FoodType.WILD_BOAR, 10, 20, false);
+
+        assertEquals(FoodType.WILD_BOAR, food.getFoodType());
+        assertEquals(10, food.getHealthEffect());
+        assertEquals(20, food.getHungerEffect());
+        assertFalse(food.isVege());
+    }
+
+    @Test
+    void testFoodStatsCreation() {
+        Food wildBoar = FoodStats.newFood(FoodType.WILD_BOAR);
+
+        assertEquals(FoodType.WILD_BOAR, wildBoar.getFoodType());
+        assertEquals(10, wildBoar.getHealthEffect());
+        assertEquals(20, wildBoar.getHungerEffect());
+    }
+
+    @Test
+    void testVegeDetection() {
+        Food carrot = FoodStats.newFood(FoodType.CARROT);
+        assertTrue(carrot.isVege());
+
+        Food lobster = FoodStats.newFood(FoodType.LOBSTER);
+        assertFalse(lobster.isVege());
+    }
+}
