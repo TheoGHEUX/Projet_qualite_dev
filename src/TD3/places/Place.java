60abfa9fbe9f32f1e54ac819584253218f27c00e
Alitalie <ali.uysun@etu.univ-amu.fr>
2025-12-05 22:34:13 +0100

    amelioration des lieux et ajout de la gestion de soin, de food, et de deplacement des personnages

--- src/TD3/places/Place.java
+++ src/TD3/places/Place.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public abstract class Place {
     protected String name;
     protected int surface; //superficie
-    protected ArrayList<Character> the_characters_present; // à modifier
-    protected ArrayList<Food> the_aliments_present; // à modifier
+    protected ArrayList<Character> the_characters_present;
+    protected ArrayList<Food> the_aliments_present;
 
     public Place(String name, int surface) {
         this.name = name;
@@ -20,61 +20,13 @@ public abstract class Place {
 
     public abstract boolean canAccept(Character character);
 
-    //Getters
-    public String getName() {
-        return name;
-    }
+    //GESTION DES PERSONNAGES
 
-    public int getSurface() {
-        return surface;
-    }
-
-    //maxCapacity a implementer
-
-    public ArrayList<Character> getThe_characters_present() {
-        return the_characters_present;
-    }
-    public ArrayList<Food> getThe_aliments_present() {
-        return the_aliments_present;
-    }
-
-    public void showPlaceInfos() {
-        System.out.println("=== " + name + " ===");
-        System. out.println("Surface: " + surface + " m² | Capacité: " + getNumberOfCharacters());
-
-        if (this instanceof Place_with_clan_chief) {
-            System.out.println("Chef: " + ((Place_with_clan_chief) this).getClanChief());
-        }
-
-        System.out.println("\nPersonnages (" + getNumberOfCharacters() + ") :");
-        if (the_characters_present.isEmpty()) {
-            System.out.println("  (aucun)");
-        } else {
-            for (Character c : the_characters_present) {
-                System.out.println("  - " + c.getName() + " (" + c.getType() + ") - " + c.getHealth() + " PV"); //pour affichage clair
-            }
-        }
-
-        System.out.println("\nAliments (" + getNumberOfFood() + ") :");
-        if (the_aliments_present.isEmpty()) {
-            System.out.println("  (aucun)");
-        } else {
-            for (Food f : the_aliments_present) {
-                System.out.println("  - " + f.getFoodType() + " (+" + f.getHealthEffect() + " PV)"); //pour affichage clair
-            }
-        }
-        System.out.println();
-    }
-
-
-    public int getNumberOfCharacters() {
-        return the_characters_present.size();
-    }
-
-    public int getNumberOfFood() {
-        return the_aliments_present.size();
-    }
-
+    /**
+     * Ajoute un personnage au lieu s'il peut y entrer
+     * @param character le personnage à ajouter
+     * @return true si ajouté avec succès, false sinon
+     */
     public boolean addCharacter(Character character) {
         if (character == null) {
             System.out.println("Erreur : personnage null");
@@ -87,26 +39,201 @@ public abstract class Place {
         }
 
         the_characters_present.add(character);
-        System.out.println(character. getName() + " entre dans " + this.name);
+        System.out.println(character.getName() + " entre dans " + this.name);
         return true;
     }
 
+    /**
+     * Retire un personnage du lieu
+     * @param character le personnage à retirer
+     * @return true si retiré, false sinon
+     */
     public boolean removeCharacter(Character character) {
         if (the_characters_present.remove(character)) {
-            System.out.println(character.getName() + " quitte " + this.name);
+            System.out. println(character.getName() + " quitte " + this.name);
             return true;
         }
         System.out.println(character.getName() + " n'est pas dans " + this.name);
         return false;
     }
 
+    /**
+     * Vérifie si un personnage est présent dans le lieu
+     */
     public boolean containsCharacter(Character character) {
-        return the_characters_present. contains(character);
+        return the_characters_present.contains(character);
+    }
+
+    //SOINS
+
+    /**
+     * Soigne un personnage spécifique d'un montant donné
+     * @param character le personnage à soigner
+     * @param healAmount la quantité de vie à restaurer
+     * @return true si soigné, false si le personnage n'est pas présent
+     */
+    public boolean healCharacter(Character character, int healAmount) {
+        if (! containsCharacter(character)) {
+            System.out.println(character.getName() + " n'est pas dans " + this.name);
+            return false;
+        }
+
+        if (! character.isAlive()) {
+            System.out.println(character. getName() + " est mort, impossible de le soigner");
+            return false;
+        }
+
+        int oldHealth = character.getHealth();
+        character.updateHealth(healAmount);
+        int newHealth = character.getHealth();
+
+        System.out.println(character.getName() + " a été soigné de " + (newHealth - oldHealth) + " PV dans " + this.name);
+        return true;
+    }
+
+
+    //NOURIR
+
+    /**
+     * Nourrit un personnage avec un aliment spécifique du lieu
+     * @param character le personnage à nourrir
+     * @param foodIndex l'index de l'aliment dans la liste
+     * @return true si nourri avec succès, false sinon
+     */
+    public boolean feedCharacter(Character character, int foodIndex) {
+        // Vérifier que le personnage est présent
+        if (!containsCharacter(character)) {
+            System.out.println(character.getName() + " n'est pas dans " + this.name);
+            return false;
+        }
+
+        // Vérifier que le personnage est vivant
+        if (!character.isAlive()) {
+            System.out.println(character.getName() + " est mort, impossible de le nourrir");
+            return false;
+        }
+
+        // Vérifier qu'il y a de la nourriture
+        if (the_aliments_present.isEmpty()) {
+            System.out.println("Aucune nourriture disponible dans " + this.name);
+            return false;
+        }
+
+        // Vérifier l'index
+        if (foodIndex < 0 || foodIndex >= the_aliments_present.size()) {
+            System.out.println("Index de nourriture invalide");
+            return false;
+        }
+
+        // Nourrir le personnage
+        Food food = the_aliments_present.get(foodIndex);
+        System.out.println(character.getName() + " mange " + food.getFoodType() + " dans " + this.name);
+        character.eat(food);
+
+        // Retirer l'aliment consommé
+        the_aliments_present.remove(foodIndex);
+
+        return true;
     }
 
 
 
 
+    /**
+     * Vérifie s'il y a des personnages ayant faim (< 100)
+     */
+    private boolean hasHungryCharacters() {
+        for (Character c : the_characters_present) {
+            if (c.isAlive() && c.getHunger() < 100) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    //GESTION DES ALIMENTS
 
+    /**
+     * Ajoute un aliment au lieu
+     */
+    public boolean addFood(Food food) {
+        if (food == null) {
+            System. out.println("Erreur : aliment null");
+            return false;
+        }
+        the_aliments_present.add(food);
+        System.out. println(food.getFoodType() + " ajouté à " + this.name);
+        return true;
+    }
+
+    /**
+     * Retire un aliment du lieu
+     */
+    public boolean removeFood(int index) {
+        if (index < 0 || index >= the_aliments_present.size()) {
+            System.out.println("Index invalide");
+            return false;
+        }
+        Food removed = the_aliments_present. remove(index);
+        System. out.println(removed.getFoodType() + " retiré de " + this.name);
+        return true;
+    }
+
+    // ========== GETTERS ==========
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSurface() {
+        return surface;
+    }
+
+    public ArrayList<Character> getThe_characters_present() {
+        return the_characters_present;
+    }
+
+    public ArrayList<Food> getThe_aliments_present() {
+        return the_aliments_present;
+    }
+
+    public int getNumberOfCharacters() {
+        return the_characters_present.size();
+    }
+
+    public int getNumberOfFood() {
+        return the_aliments_present.size();
+    }
+
+    // AFFICHAGE
+
+    public void showPlaceInfos() {
+        System.out.println("=== " + name + " ===");
+        System.out.println("Surface: " + surface + " m² | Personnages: " + getNumberOfCharacters() + " | Aliments: " + getNumberOfFood());
+
+        if (this instanceof Place_with_clan_chief) {
+            System.out. println("Chef: " + ((Place_with_clan_chief) this).getClanChief());
+        }
+
+        System.out.println("\nPersonnages (" + getNumberOfCharacters() + ") :");
+        if (the_characters_present.isEmpty()) {
+            System.out. println("  (aucun)");
+        } else {
+            for (Character c : the_characters_present) {
+                System.out.println("  - " + c.getName() + " (" + c.getType() + ") - "
+                        + c.getHealth() + "/" + c.getMaxHealth() + " PV | Faim: " + c.getHunger() + "%");
+            }
+        }
+
+        System.out.println("\nAliments (" + getNumberOfFood() + ") :");
+        if (the_aliments_present.isEmpty()) {
+            System.out.println("  (aucun)");
+        } else {
+            for (Food f : the_aliments_present) {
+                System.out.println("  - " + f.getFoodType() + " (+" + f.getHealthEffect() + " PV, +"
+                        + f.getHungerEffect() + " faim)");
+            }
+        }
+        System.out.println();
+    }
 }
--- src/TD3/places/Roman_fortified_camp.java
+++ src/TD3/places/Roman_fortified_camp.java
@@ -3,6 +3,7 @@ package TD3.places;
 import TD3.characters.Character;
 import TD3.characters.roman.Roman;
 import TD3.characters.creature.Creature;
+import TD3.interfaces.Fighter;
 
 //camp retranché romain
 public class Roman_fortified_camp extends Place_with_clan_chief {
@@ -13,7 +14,7 @@ public class Roman_fortified_camp extends Place_with_clan_chief {
 
     public boolean canAccept(Character character) {
         // Accepte seulement les combattants romains et les créatures magiques
-        return ((character instanceof Roman))        // ############# conditions à rajouter : est combattants
+        return ((character instanceof Roman) && (character instanceof Fighter))
                 || (character instanceof Creature);
     }
 }
