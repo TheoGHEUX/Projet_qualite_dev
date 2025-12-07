d3d0b2e0b2c4bfa8a0d6701cdfe5e702ac3c7fc1
GHEUX Théo <theoxghx@gmail.com>
2025-12-07 02:25:47 +0100

    Ajout de la recette de la potion de base

---
+++ src/TD3/potion/PotionRecipe.java
@@ -0,0 +1,27 @@
+package TD3.potion;
+
+import TD3.enums.FoodType;
+import TD3.food.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PotionRecipe {
+    public List<FoodType> recipe = new ArrayList<>();
+
+    public PotionRecipe() {
+        recipe.add(FoodType.MISTLETOE);
+        recipe.add(FoodType.CARROT);
+        recipe.add(FoodType.SALT);
+        recipe.add(FoodType.FRESH_FOUR_LEAF_CLOVER);
+        recipe.add(FoodType.PASSABLE_FRESH_FISH);
+        recipe.add(FoodType.ROCKFISH_OIL);
+        recipe.add(FoodType.HONEY);
+        recipe.add(FoodType.MEAD);
+        recipe.add(FoodType.SECRET_INGREDIENT);
+    }
+
+    public List<FoodType> getRecipe() {
+        return recipe;
+    }
+}
