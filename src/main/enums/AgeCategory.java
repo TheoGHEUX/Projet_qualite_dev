f00c3d7be06360655d8f7e24787f299224ef4e7d
GHEUX <theo.gheux@etu.univ-amu.fr>
2025-12-10 11:23:33 +0100

    Ajouts d'enums pour la catégorie de l'age du loup garou et du rang de domination

---
+++ src/main/enums/AgeCategory.java
@@ -0,0 +1,10 @@
+package main.enums;
+
+/**
+ * Les catégories d'age d'un lycanthrope
+ */
+public enum AgeCategory {
+    YOUNG,
+    ADULT,
+    OLD
+}
---
+++ src/main/enums/DominationRank.java
@@ -0,0 +1,10 @@
+package main.enums;
+
+public enum DominationRank {
+    ALPHA,
+    BETA,
+    GAMMA,
+    DELTA,
+    EPSILON,
+    OMEGA
+}
